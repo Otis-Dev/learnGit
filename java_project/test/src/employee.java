fc6f02d94353831f990a57f2b2d6be39a68b6970
mydevops <mydevops1912@gmail.com>
2024-06-14 23:46:55 +0700

    add java project folder

---
+++ java_project/test/src/employee.java
@@ -0,0 +1,16 @@
+public class employee {
+    String ID;
+    String fullName;
+    int yearOfBirth;
+    int salary;
+
+    public employee(String ID, String fullName, int yearOfBirth, int salary) {
+        this.ID = ID;
+        this.fullName = fullName;
+        this.yearOfBirth = yearOfBirth;
+        this.salary = salary;
+    }
+
+    public employee() {
+    }
+}
