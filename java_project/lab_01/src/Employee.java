58879f973e2164e2441d24abbe31a22fbd97e2e3
mydevops <mydevops1912@gmail.com>
2024-06-15 08:44:55 +0700

    update lab1 with basic function

---
+++ java_project/lab_01/src/Employee.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+
+public class Employee {
+    private String id;
+    private String fullName;
+    private int yearOfBirth;
+    private int salary;
+
+    public Employee(){
+        this.id = "";
+        this.fullName = "";
+        this.yearOfBirth = 0;
+        this.salary = 0;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public void input(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Employee ID: ");
+        id = sc.nextLine();
+        System.out.print("\nEnter Employee Full Name: ");
+        fullName = sc.nextLine();
+        System.out.print("\nEnter Employee Year Of Birth: ");
+        yearOfBirth = sc.nextInt();
+        System.out.print("\nEnter Employee Salary: ");
+        salary = sc.nextInt();
+    }
+
+    public void output(){
+        System.out.println("--- List of employee ---");
+        System.out.println("- No. -- ID -- Employee name -- Year Of Birth -- Salary -");
+        System.out.println("- " + id + " -- " + fullName + " -- " + yearOfBirth + " -- " + salary +" -");
+    }
+}
---
+++ java_project/lab_01/src/JavaApplication.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class JavaApplication {
+    public static void main(String[] args) {
+        Employee emp = new Employee();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("1. Adds new employee.");
+        System.out.println("2. Show all employees.");
+        System.out.println("3. Quit");
+        System.out.print("Please choice your option: ");
+        int choice = sc.nextInt();
+        switch (choice) {
+            case 1:
+                emp.input();
+                emp.output();
+                break;
+            case 2:
+                emp.output();
+                break;
+            case 3:
+                System.exit(0);
+            default:
+                System.out.println("Invalid choice.");
+        }
+    }
+}
