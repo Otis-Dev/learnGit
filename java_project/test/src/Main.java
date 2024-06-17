6cb638c467d3a6c0ebe50239439304bc311e9c6a
mydevops <mydevops1912@gmail.com>
2024-06-17 21:25:39 +0700

    test list function

--- java_project/test/src/Main.java
+++ java_project/test/src/Main.java
@@ -1,27 +1,46 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 import java.util.Scanner;
-class func {
-    Scanner sc = new Scanner(System.in);
-    public int checkInt(){
-        do{
-            try {
-                String check = sc.nextLine().trim().replaceAll("\\s+", "");
-                int num = Integer.parseInt(check);
-                return num;
-            } catch ( NumberFormatException e){
-                System.err.println("Please enter a valid number.");
-            }
-        }while (true);
-    }
-}
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+//class func {
+//    Scanner sc = new Scanner(System.in);
+//
+//    public int checkInt() {
+//        do {
+//            try {
+//                String check = sc.nextLine().trim().replaceAll("\\s+", "");
+//                int num = Integer.parseInt(check);
+//                return num;
+//            } catch (NumberFormatException e) {
+//                System.err.println("Please enter a valid number.");
+//            }
+//        } while (true);
+//    }
+//
+//
+//}
+
+class infor{
+    Scanner sc=new Scanner(System.in);
+    String name;
+    int id;
+
+    public void  input(){
+
+        System.out.print("Enter Name: ");
+        name=sc.nextLine();
+        System.out.print("Enter ID: ");
+        id = sc.nextInt();
+    }
+
+    public void output(){
+
+    }
+}//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it
-        func f = new func();
-        int a = f.checkInt();
-        System.out.println(a);
+
     }
 }
